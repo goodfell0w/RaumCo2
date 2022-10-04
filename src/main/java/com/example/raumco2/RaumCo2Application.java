25a38c594d9a30bd151719351a4d9c5cc6209aa1
denis lischer <denislischer@hotmail.com>
2022-10-04 15:33:38 +0200

    Runnable Version with Sending

--- src/main/java/com/example/raumco2/RaumCo2Application.java
+++ src/main/java/com/example/raumco2/RaumCo2Application.java
@@ -6,25 +6,31 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.time.LocalTime;
 import java.util.Timer;
 
+import static java.lang.Thread.sleep;
 import static java.time.LocalTime.now;
 
 
 @SpringBootApplication
 public class RaumCo2Application {
 
-    public static void main(String[] args) {
-        SpringApplication.run(RaumCo2Application.class, args);
-    }{
+    public static void main(String[] args) throws InterruptedException {
+
         Room e004 = new Room("e004", -1.0);
         Room e003 = new Room("e003", -1.0);
 
+        System.out.println("Start");
         LocalTime startTime = now().plusMinutes(5);
+        System.out.println("Time started");
+        Double temp = 0.0;
+        while(now().compareTo(startTime) < 0) {
+            System.out.println("While entered");
+            e004.setCo2Value(500.2 + temp );
+            e004.publishCo2Value();
+            sleep(5000);
+            temp += 5.0;
 
-        while(now().compareTo(startTime) > 0){
-        e004.setCo2Value(500.2);
-        e004.publishCo2Value();
-        e004.setCo2Value(300.3);
-        e004.publishCo2Value();
+
+
         }
     }
 }
--- src/main/java/com/example/raumco2/Room.java
+++ src/main/java/com/example/raumco2/Room.java
@@ -7,9 +7,18 @@ import com.rabbitmq.client.ConnectionFactory;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.Objects;
 import java.util.concurrent.TimeoutException;
 
-public record Room(String name, Double co2Value) {
+public final class Room {
+    private final String name;
+    public Double co2Value;
+
+    public Room(String name, Double co2Value) {
+        this.name = name;
+        this.co2Value = co2Value;
+    }
+
     public Double getCo2Value() {
         return this.co2Value;
     }
@@ -19,11 +28,11 @@ public record Room(String name, Double co2Value) {
      * @param co2Value
      * @return Returns a new Room Object with the updated co2Value
      */
-    public Room setCo2Value(Double co2Value){
-        return new Room(this.name, co2Value);
+    public void setCo2Value(Double co2Value) {
+        this.co2Value = co2Value;
     }
 
-    public void publishCo2Value(){
+    public void publishCo2Value() {
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("localhost");
         try (Connection connection = factory.newConnection();
@@ -37,4 +46,34 @@ public record Room(String name, Double co2Value) {
             throw new RuntimeException(e);
         }
     }
+
+    public String name() {
+        return name;
+    }
+
+    public Double co2Value() {
+        return co2Value;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (obj == null || obj.getClass() != this.getClass()) return false;
+        var that = (Room) obj;
+        return Objects.equals(this.name, that.name) &&
+                Objects.equals(this.co2Value, that.co2Value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, co2Value);
+    }
+
+    @Override
+    public String toString() {
+        return "Room[" +
+                "name=" + name + ", " +
+                "co2Value=" + co2Value + ']';
+    }
+
 }
